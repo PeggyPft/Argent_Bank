6b3590fc53cc885a05aeeb485b582e635cd3acc7
peggyp <proffit.peggy@yahoo.fr>
2024-06-04 16:12:36 +0200

    Adding user login

--- Front-end/src/components/header/Header.jsx
+++ Front-end/src/components/header/Header.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import logoBanner from '../../assets/img/argentBankLogo.png';
 import {Link} from 'react-router-dom';
+import {logout} from '../../Slices/userSlice';
 
 const Header = () => {
+    const dispatch = useDispatch();
+    const userName = useSelector((state) => state.user.user.userName);
+    const isAuthenticated = !!useSelector((state) => state.user.token);
+
+    const handleSignOut = () => {
+        dispatch(logout());
+    };
+
     return (
         <div className='main-nav'>
             <Link to="/" className='main-nav-logo'><img src={logoBanner} alt="Logo Argent Bank" className='main-nav-logo-image'/>
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
-            <Link to="/sign-in" className='main-nav-item'><i className="fa fa-user-circle"> <span>Sign In</span> </i></Link>
+            {isAuthenticated ? (
+                <div>
+                    <i className="fa fa-user-circle"> <span>{userName}</span></i>
+                    <Link to="/" onClick={handleSignOut} > <i className='fa fa-sign-out'> <span>Sign Out</span></i></Link>
+                </div>
+            ) : (
+                <Link to="/sign-in" className='main-nav-item'><i className="fa fa-user-circle"> <span>Sign In</span> </i></Link>
+            )}
         </div>
-
     );
 };
 
